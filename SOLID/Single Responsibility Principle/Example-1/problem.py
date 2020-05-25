cac27ea1612ed65aeba21fcfddef4799d960f2c6
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-25 17:16:15 +0530

    Single Responsibility Problem

---
+++ SOLID/Single Responsibility Principle/Example-1/problem.py
@@ -0,0 +1,42 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+
+    # Main responsibility of the journal class 
+    # is to write and remove enteries.
+    # By adding extra functionality like saving and loading journal
+    # we're breaking the SRP.
+    def save(self, filename):
+        file = open(filename, "w")
+        file.write(str(self))
+        file.close()
+
+    def load(self, filename):
+        pass
+
+    def load_from_web(self, uri):
+        pass
+
+
+j = Journal()
+j.add_entry("I cried today.")
+j.add_entry("I ate a bug.")
+print(f"Journal entries:\n{j}\n")
+
+file = "journal.txt"
+j.save(file)
+
+print("Inside Journal File")
+with open(file) as fh:
+    print(fh.read())
