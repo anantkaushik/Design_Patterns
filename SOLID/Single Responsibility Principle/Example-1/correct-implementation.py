ae3381e25bf6a8751320b103db5d6b23c3cc2369
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-25 17:24:07 +0530

    Single Rety Principle

---
+++ SOLID/Single Responsibility Principle/Example-1/correct-implementation.py
@@ -0,0 +1,37 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+
+
+# By separating the functionality of saving and loading 
+# teh journal we're maintaining the SRP.
+class PersistenceManager:
+    def save_to_file(self, journal, filename):
+        file = open(filename, "w")
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry("I cried today.")
+j.add_entry("I ate a bug.")
+print(f"Journal entries:\n{j}\n")
+
+p = PersistenceManager()
+file = 'journal.txt'
+p.save_to_file(j, file)
+
+print("Inside Journal File")
+with open(file) as fh:
+    print(fh.read())
