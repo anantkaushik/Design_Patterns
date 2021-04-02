2be72ce02cfbde2a1e47dab59d22a1f5e312302f
Anant Kaushik <anant.kaushik2@gmail.com>
2021-04-02 20:40:36 +0530

    Chain of Responsibility

---
+++ Chain_Of_Responsibiltiy/README.md
@@ -0,0 +1,11 @@
+# Chain of Responsibility
+> Chain of Responsibility is a behavioral design pattern that lets you pass requests along a chain of handlers. 
+> Upon receiving a request, each handler decides either to process the request or to pass it to the next handler 
+> in the chain.
+
+
+### Applicability:
+- Use the Chain of Responsibility pattern when your program is expected to process different kinds of requests in 
+  various ways, but the exact types of requests and their sequences are unknown beforehand.
+- Use the pattern when it’s essential to execute several handlers in a particular order.
+
---
+++ Chain_Of_Responsibiltiy/example.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    """
+    The Handler interface declares a method for building the chain of handlers.
+    It also declares a method for executing a request.
+    """
+
+    @abstractmethod
+    def set_next(self, handler):
+        pass
+
+    @abstractmethod
+    def handle(self, request):
+        pass
+
+
+class AbstractHandler(Handler):
+    """
+    The default chaining behavior can be implemented inside a base handler
+    class.
+    """
+
+    _next_handler: Handler = None
+
+    def set_next(self, handler):
+        self._next_handler = handler
+        # Returning a handler from here will let us link handlers in a
+        # convenient way like this:
+        # monkey.set_next(squirrel).set_next(dog)
+        return handler
+
+    @abstractmethod
+    def handle(self, request):
+        if self._next_handler:
+            return self._next_handler.handle(request)
+
+        return None
+
+
+"""
+All Concrete Handlers either handle a request or pass it to the next handler in
+the chain.
+"""
+
+
+class MonkeyHandler(AbstractHandler):
+    def handle(self, request):
+        if request == "Banana":
+            return f"Monkey: I'll eat the {request}"
+        return super().handle(request)
+
+
+class SquirrelHandler(AbstractHandler):
+    def handle(self, request):
+        if request == "Nut":
+            return f"Squirrel: I'll eat the {request}"
+        return super().handle(request)
+
+
+class DogHandler(AbstractHandler):
+    def handle(self, request):
+        if request == "MeatBall":
+            return f"Dog: I'll eat the {request}"
+        return super().handle(request)
+
+
+def client_code(handler: Handler):
+    """
+    The client code is usually suited to work with a single handler. In most
+    cases, it is not even aware that the handler is part of a chain.
+    """
+
+    for food in ["Nut", "Banana", "Cup of coffee"]:
+        print(f"Client: Who wants a {food}?")
+        result = handler.handle(food)
+        if result:
+            print(f"  {result}", end="")
+        else:
+            print(f"  {food} was left untouched.", end="")
+
+
+if __name__ == "__main__":
+    monkey = MonkeyHandler()
+    squirrel = SquirrelHandler()
+    dog = DogHandler()
+
+    monkey.set_next(squirrel).set_next(dog)
+
+    # The client should be able to send a request to any handler, not just the
+    # first one in the chain.
+    print("Chain: Monkey > Squirrel > Dog")
+    client_code(monkey)
+    print("\n")
+
+    print("Subchain: Squirrel > Dog")
+    client_code(squirrel)
